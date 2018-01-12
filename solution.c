0026e1a9c9330c5786fbd2218efb916847d1854b
Calvin Ho <clho@e5-05-15.ece.ualberta.ca>
2018-01-12 10:27:29 -0700

    solution file added

---
+++ solution.c
@@ -0,0 +1,35 @@
+//notes: matrixgen -> data.input -> solution -> data.output -> serialtester
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+#include <lab1_IO.h>
+
+const int MAX_THREADS = 64;
+
+int thread_count;
+
+
+int main (int argc, char* argv[])
+{
+
+
+long thread;
+
+pthread_t* thread_handles;
+
+if (argc < 2)
+{
+   printf("not enough parameters");
+    return 0;
+}
+
+thread_count = strtol(argv[1], NULL, 10);
+
+thread_handles = malloc(thread_count*sizeof(pthread_t));
+
+
+
+}
+
